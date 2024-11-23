d9b29c6bf9a057014b24c55c9d354f07b8fa8ab2
lilalajemima <169045583+lilalajemima@users.noreply.github.com>
2024-11-23 14:54:55 +0200

    Set up for the terminal based version and the web version

---
+++ savey_terminal/savey.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Imports will go here
+
+#The Api will go here 
+
+#The function for getting input will be here 
+
+#The function for the results will be 
+
+#The function for trhe interface will be here
